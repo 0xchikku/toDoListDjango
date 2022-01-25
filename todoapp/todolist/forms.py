1722f21089f45b34628fb87d693177de87657b35
Ritesh Dubey <83487783+0xchikku@users.noreply.github.com>
2022-01-25 09:03:24 +0530

    Todolist delete

--- todoapp/todolist/forms.py
+++
@@ -1,9 +0,0 @@
-from pyexpat import model
-from django import forms
-from django.forms import ModelForm
-from .models import Task
-
-class TaskForm(forms.ModelForm):
-    class Meta:
-        model = Task
-        fields = "__all__" # includes all fields in form
